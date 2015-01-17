343a4b6475eb04bdaba72beb5a6e9958fbf83b8f
neojato <kpaul86@gmail.com>
2015-01-17 09:42:42 -0600

    Step 3: Create some data

--- scripts/teamAppModule.js
+++ scripts/teamAppModule.js
@@ -1,4 +1,13 @@
 var teamApp = angular.module('myTeamApp', [])
   .controller('teamCtrl', ['$scope', function($scope) {
     $scope.pageTitle = "Team Generator";
+    $scope.people = [];
+    $scope.add = function(name) {
+      $scope.people.push(name);
+      $scope.newName = '';
+      console.log($scope);
+    };
+    $scope.remove = function(index) {
+      $scope.people.splice(index, 1);
+    };
   }]);
