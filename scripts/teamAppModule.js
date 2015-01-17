c8b0976f1da355670aa4d336f0b322b862490566
Kyle Paul <kpaul86@gmail.com>
2015-01-17 16:18:17 +0000

    Step 5: Create the teams

--- scripts/teamAppModule.js
+++ scripts/teamAppModule.js
@@ -10,4 +10,24 @@ var teamApp = angular.module('myTeamApp', [])
     $scope.remove = function(index) {
       $scope.people.splice(index, 1);
     };
+    $scope.shuffle = function(array) {
+    	for (var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
+    	return array;
+    };
+    $scope.chunk = function(array, num) {
+        var len = array.length;
+        var out = [];
+        var i = 0;
+        while (i < len) {
+            var size = Math.ceil((len - i) / num--);
+            out.push(array.slice(i, i += size));
+        }
+        return out;
+    };
+    $scope.generateTeams = function(number) {
+    	$scope.teams = [];
+    	var shuffled = $scope.shuffle(angular.copy($scope.people));
+    	$scope.teams = $scope.chunk(shuffled, number);
+    	console.log($scope.teams);
+    };
   }]);
